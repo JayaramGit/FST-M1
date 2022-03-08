46b53d996615a5a21332985aba98b733e047143b
JayaramGit <97495856+JayaramGit@users.noreply.github.com>
2022-03-08 21:59:37 +0530

    Add files via upload

---
+++ Selenium/Project/Activity2.java
@@ -0,0 +1,15 @@
+package SuiteCRM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity2 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://alchemy.hguy.co/crm");
+        String element = driver.findElement(By.tagName("img")).getAttribute("src");
+        System.out.println(element);
+        driver.close();
+    }
+}
---
+++ Selenium/Project/Activity3.java
@@ -0,0 +1,15 @@
+package SuiteCRM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity3 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://alchemy.hguy.co/crm");
+        String element = driver.findElement(By.id("admin_options")).getText();
+        System.out.println(element);
+        driver.close();
+    }
+}
